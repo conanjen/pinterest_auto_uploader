8c943795427a0b7e6dadc0dffcd09cfa60e80ea2
conan <conan@conan-laptop.(none)>
2012-02-28 01:56:10 -0800

    restyling text

--- pinterest_auto_uploader.js
+++ pinterest_auto_uploader.js
@@ -151,10 +151,10 @@ pinterest.showPanel = function(){
 		'<div id="pinterest_panel" style="position:fixed;width:250px;top:50px;right:30px;z-index:10000;background:#67A6A6;border-radius:10px;padding:15px"> \
 		<div style="background:#EDF7F9;padding:10px;width:230px;border-radius:8px"> \
 		<h1 style="font-size:20px;margin:0;padding:0;color:#67A6A6">Hello!</h1> \
-		<p style="font-size:15px;margin:0;padding:0;color:#67A6A6">Pin Daily Aisle Venues <br/>in 2 easy steps: <br/> \
+		<p style="font-size:15px;margin:0;padding:0;color:#67A6A6">Pin Daily Aisle Venues <br/>in 2 easy steps: <br/></p> \
 		<hr style="border-style:solid;border-color:#67A6A6"> \
-		1. Click this: <a href="#" id="pin_images" style="display:block;margin:10px 0;padding:10px;border-radius:8px;background:#E7259D;color:white">Start pinning!</a> <br/> \
-		2. Wait a few minutes for the spinner to work its magic.  Feel great for helping your friends.</p> \
+		<p style="font-size:15px;margin:0;padding:0;color:#67A6A6">1. Click this: <a href="#" id="pin_images" style="display:block;margin:10px 0;padding:10px;border-radius:8px;background:#E7259D;color:white">Start pinning!</a></p> <br/> \
+		<p style="font-size:15px;margin:0;padding:0;color:#67A6A6">2. Wait a few minutes for the spinner to work its magic.  Feel great for helping your friends.</p> \
 		<hr style="border-style:solid;border-color:#67A6A6"> \
 		<img id="loader_gif" src="http://media.dailyaisle.com/media/img/ajax-loader.gif" width="50" height="50"/> \
 		<span id="counter" style:"color:#67A6A6"></span> \
