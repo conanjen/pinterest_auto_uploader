01117ef4314221d3cc3cf7a90aaf1fc9ebb43290
conan <conan@conan-laptop.(none)>
2012-03-01 14:21:48 -0800

    increasing image counter size

--- pinterest_auto_uploader.js
+++ pinterest_auto_uploader.js
@@ -152,7 +152,7 @@ pinterest.showPanel = function(){
 		<p style="font-size:15px;margin:0;padding:0;color:#67A6A6">2. Wait a few minutes for the uploader to work its magic.  Feel great for helping your friends.</p> \
 		<hr style="border-style:solid;border-color:#67A6A6"> \
 		<img id="loader_gif" style="display:none" src="http://media.dailyaisle.com/media/img/ajax-loader.gif" width="50" height="50"/> \
-		<span id="counter" style:"color:#67A6A6"></span> \
+		<span id="counter" style:"color:#67A6A6;font-size:15px"></span> \
 		</div></div>';
 	$('body').append(html);
 	var $panel = $('#pinterest_panel');
